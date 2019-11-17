5c814de929db4c0ba8f0a262dceddb97f6b2b77f
Flo Schuessel <fs@iprods.org>
2019-11-17 16:33:22 +0100

    Add link to repo

--- src/ui/renderer.ts
+++ src/ui/renderer.ts
@@ -1,12 +1,21 @@
-import {remote} from 'electron'
-import '../../assets/base.css'
-import Screen from '../modules/screen'
+import {remote, shell} from 'electron';
+import '../../assets/base.css';
+import Screen from '../modules/screen';
 
 const app = remote.app
+
+const openLinkInBrowser = (e: Event) => {
+  const el = e.target as HTMLElement
+  shell.openExternal(el.dataset.repoLink)
+};
 
 const versionElement = document.querySelector('#version')
 if (versionElement) {
   versionElement.textContent = `v${app.getVersion()}`
+}
+const repoLinkElement = document.querySelector('#repoLink')
+if (repoLinkElement) {
+  repoLinkElement.addEventListener('click', openLinkInBrowser)
 }
 
 Screen.init(document)
