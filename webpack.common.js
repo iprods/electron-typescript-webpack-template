e713a7b4577400594fae24879f87befcfb923b45
Flo Schuessel <fs@iprods.org>
2019-11-19 00:21:51 +0100

    ImproveWebpack setup

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "[name].js",
   },
   node: {
